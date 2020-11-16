27ebd7c190fafdaefd85e721a28b4a5e31f110a2
Chih-Yuan Su <csu68@gatech.edu>
2020-11-15 23:36:35 -0800

    update Workout.js

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -6,8 +6,13 @@ const Workout = () => {
   const [isActive, setIsActive] = useState(false);
   const [counting, setCounting] = useState(null);
   const [timer, setTimer] = useState(defaultTimer);
-  const [complete, setComplete] = useState("");
+  const [curlUp, setCurlUp] = useState(false);
+  const [pushUp, setPushUp] = useState(false);
+  const [jumpingJacks, setJumpingJacks] = useState(false);
+  const [squat, setSquat] = useState(false);
+  const [plankJacks, setPlankJacks] = useState(false);
 
+  const complete = curlUp && pushUp && jumpingJacks && squat && plankJacks;
   const tbodyCls = "col-sm mt-2 mb-2";
   const theadCls = "col-sm mt-3 mb-3 font-weight-bold";
 
@@ -19,7 +24,6 @@ const Workout = () => {
       }, 1000);
     } else if (timer === 0) {
       clearInterval(interval);
-      setComplete(complete + counting + " Done!");
       reset();
     }
     return () => clearInterval(interval);
@@ -65,29 +69,95 @@ const Workout = () => {
         <div className="row">
           <div className={theadCls}>Tasks</div>
           <div className={theadCls}>Timer</div>
+          <div className={theadCls}>Mark Done</div>
         </div>
         <div className="row">
           <div className={tbodyCls}>2 Minute Curl Up</div>
-          <div className={tbodyCls}>{renderTimer("Curl Up")}</div>
+          <div className={tbodyCls}>
+            {curlUp ? null : renderTimer("Curl Up")}
+          </div>
+          <div className={tbodyCls}>
+            {curlUp ? (
+              <p>DONE</p>
+            ) : (
+              <button
+                className="workout-button"
+                onClick={() => setCurlUp(true)}
+              >
+                Click to Mark Curl Up DONE
+              </button>
+            )}
+          </div>
         </div>
         <div className="row">
           <div className={tbodyCls}>2 Minute Push Up</div>
-          <div className={tbodyCls}>{renderTimer("Push Up")}</div>
+          <div className={tbodyCls}>
+            {pushUp ? null : renderTimer("Push Up")}
+          </div>
+          <div className={tbodyCls}>
+            {pushUp ? (
+              <p>DONE</p>
+            ) : (
+              <button
+                className="workout-button"
+                onClick={() => setPushUp(true)}
+              >
+                Click to Mark Push Up DONE
+              </button>
+            )}
+          </div>
         </div>
         <div className="row">
           <div className={tbodyCls}>2 Minute Jumping Jacks</div>
-          <div className={tbodyCls}>{renderTimer("Jumping Jacks")}</div>
+          <div className={tbodyCls}>
+            {jumpingJacks ? null : renderTimer("Jumping Jacks")}
+          </div>
+          <div className={tbodyCls}>
+            {jumpingJacks ? (
+              <p>DONE</p>
+            ) : (
+              <button
+                className="workout-button"
+                onClick={() => setJumpingJacks(true)}
+              >
+                Click to Mark Jumping Jacks DONE
+              </button>
+            )}
+          </div>
         </div>
         <div className="row">
           <div className={tbodyCls}>2 Minute Squat</div>
-          <div className={tbodyCls}>{renderTimer("Squat")}</div>
+          <div className={tbodyCls}>{squat ? null : renderTimer("Squat")}</div>
+          <div className={tbodyCls}>
+            {squat ? (
+              <p>DONE</p>
+            ) : (
+              <button className="workout-button" onClick={() => setSquat(true)}>
+                Click to Mark Squat DONE
+              </button>
+            )}
+          </div>
         </div>
         <div className="row">
           <div className={tbodyCls}>2 Minute Plank Jacks</div>
-          <div className={tbodyCls}>{renderTimer("Plank Jacks")}</div>
+          <div className={tbodyCls}>
+            {plankJacks ? null : renderTimer("Plank Jacks")}
+          </div>
+          <div className={tbodyCls}>
+            {plankJacks ? (
+              <p>DONE</p>
+            ) : (
+              <button
+                className="workout-button"
+                onClick={() => setPlankJacks(true)}
+              >
+                Click to Mark Plank Jacks DONE
+              </button>
+            )}
+          </div>
         </div>
       </div>
-      <div className="mt-3">{complete}</div>
+      <div className="mt-3">{complete ? <p>Tasks Done!</p> : null}</div>
     </div>
   );
 };
